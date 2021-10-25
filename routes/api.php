7103d43aa979667855f1d41c73eaa2d80a74e088
luydjmix2 <luydjmix@hotmail.com>
2021-10-25 11:29:32 -0500

    correcion de error

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register',[\App\Http\Controllers\Users\UserController::class, 'register']);
+Route::post('login', [\App\Http\Controllers\Users\UserController::class, 'authenticate']);
+
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+
+    Route::post('user',[\App\Http\Controllers\Users\UserController::class, 'getAuthenticatedUser']);
+
+});
